44f9f3b6cf28a099c6135a9c7a6a0e6fb183b90f
mahdi niknejad <68367445+mahdiniknejad@users.noreply.github.com>
2021-02-17 10:57:06 +0330

    Update settings.py

--- config/config/settings.py
+++ config/config/settings.py
@@ -146,8 +146,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "mmahdiniknejad@gmail.com"
-EMAIL_HOST_PASSWORD = "mn555mesi2302"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 
 # from decouple import config
 
